5140625e0671426f2fabf3de7c593905f1b123bb
THE-REK <eren83086@gmail.com>
2020-10-30 21:32:42 +0300

    todolistapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
+import {connect} from "react-redux"
+import {listeyeEkle, isaretle, temizle} from "./actions/actionTypes"
 import './App.css';
+import {Card, CardBody, CardHeader, } from "reactstrap"
 
-function App() {
+const App=(props)=> {
+  const [text, setText]=useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" > 
+    <h1>TO DO LIST
+    </h1>
+    
+    <div className="ekleme_formu">
+      <Card style={{width:"300px", marginLeft:"570px", border:"2px solid black"}}>
+        <CardHeader >
+  <input placeholder="What do you desire ?" value={text} onChange={e=>setText(e.target.value) } />
+        <button onClick={()=>{
+          setText("")
+          props.listeyeEkle(text)}}>+</button>
+        </CardHeader>
+        <CardBody style={{backgroundColor:"orange"}}>
+          <div className="liste">
+      {props.liste.map(item =>(
+        <div onClick={()=>props.isaretle(item.id)} key={item.id} style={{color:"white"}} className={item.tamamlandi?"yapildi":""}>
+          <strong><h5>
+            {item.baslik}
+          </h5>
+            
+          </strong>
+          </div>
+      ) )}
     </div>
+      
+    
+        </CardBody>
+        <button onClick={()=>props.temizle()} className="temizle">Delete The Completed</button>
+      </Card>
+      </div>
+    </div>
+    
   );
 }
 
-export default App;
+
+const mapStateToProps= state=>{
+  return {
+    liste:state.liste
+  }
+}
+
+export default connect(mapStateToProps,{listeyeEkle, isaretle, temizle} )(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from "redux"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux"
+import { reducer } from './reducers';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
